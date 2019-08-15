afbd6636f803004f75d6c53827d00c22dfa95851
rael <rael06@hotmail.fr>
2019-08-15 16:46:17 +0200

    operandes opérateurs type transtypage cast etc...

--- ProjetCSharp/Program.cs
+++ ProjetCSharp/Program.cs
@@ -6,9 +6,26 @@ namespace ProjetCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // instruction de lecture pour retenir la fenêtre console
-            Console.Read();
+            Console.WriteLine("Veuillez saisir votre message !");
+//            string monMessage = Console.ReadLine();
+//            Console.WriteLine("Votre message est : " + monMessage);
+
+//            int x = 20;
+//            Console.WriteLine(x);
+//
+//            x = 50;
+//            Console.WriteLine(x);
+
+//            long y = 25;
+//            Console.WriteLine(x.GetType());
+//            Console.WriteLine(y.GetType());
+
+//            double z = 1234.5;
+//            int g = (int) z;
+//            Console.WriteLine(g);
+
+            DateTime d = DateTime.Now;
+            Console.WriteLine(d);
         }
     }
 }
