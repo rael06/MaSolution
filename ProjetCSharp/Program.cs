158474ffc08d7e76e177ec4c1c684a09e6e10529
rael <rael06@hotmail.fr>
2019-08-15 17:15:55 +0200

    Tableaux

--- ProjetCSharp/Program.cs
+++ ProjetCSharp/Program.cs
@@ -6,26 +6,57 @@ namespace ProjetCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez saisir votre message !");
-//            string monMessage = Console.ReadLine();
-//            Console.WriteLine("Votre message est : " + monMessage);
+            int[,] tabM = new int[2, 3];
 
-//            int x = 20;
-//            Console.WriteLine(x);
+            tabM[0, 0] = 9;
+            tabM[0, 1] = 11;
+            tabM[0, 2] = 8;
+
+            tabM[1, 0] = 2;
+            tabM[1, 1] = 44;
+            tabM[1, 2] = 67;
+
+//            Console.WriteLine(tabM.Length);
+//            Console.WriteLine(tabM[0, 1]);
+//            Console.WriteLine(tabM[1, 2]);
+
+
+            string[,] tabM2 = new string[3, 2]
+            {
+                {
+                    "un",
+                    "deux"
+                },
+                {
+                    "trois",
+                    "quatre"
+                },
+                {
+                    "cinq",
+                    "six"
+                }
+            };
+
+//            int[][] TabEscalier = new int[3][];
 //
-//            x = 50;
-//            Console.WriteLine(x);
+//            TabEscalier[0] = new int[0];
+//            TabEscalier[1] = new int[6] {1, 2, 3, 4, 5, 6};
+//            TabEscalier[2] = new int[] {1, 2, 3};
+//
+//            Console.WriteLine(TabEscalier.Length);
+//            Console.WriteLine(TabEscalier[1][3]);
+//            Console.WriteLine(TabEscalier[2][2]);
 
-//            long y = 25;
-//            Console.WriteLine(x.GetType());
-//            Console.WriteLine(y.GetType());
+            int[][] TabEscalier2 =
+            {
+                new int[] {1, 2, 3, 4, 5},
+                new int[] {1, 12, 33},
+                new int[] {5, 55, 555}
+            };
 
-//            double z = 1234.5;
-//            int g = (int) z;
-//            Console.WriteLine(g);
-
-            DateTime d = DateTime.Now;
-            Console.WriteLine(d);
+            Console.WriteLine(TabEscalier2.Length);
+            Console.WriteLine(TabEscalier2[1][2]);
+            Console.WriteLine(TabEscalier2[2][2]);
         }
     }
 }
